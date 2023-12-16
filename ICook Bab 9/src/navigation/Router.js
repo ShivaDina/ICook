94f66bf1fba1af99d757f599480fe210a5274c5b
Shiva Divanti Natasya <48375637+ShivaDina@users.noreply.github.com>
2023-12-16 16:03:22 +0700

    [Bab 9] Firebase

---
+++ ICook Bab 9/src/navigation/Router.js
@@ -0,0 +1,147 @@
+import React from 'react';
+import {createStackNavigator, TransitionPresets} from '@react-navigation/stack';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import { Home, BlogDetail, Profile, Bookmark, Search, AddBlogForm, EditBlogForm, Tulis } from '../screens';
+import {Edit, Home2, Bookmark2, ProfileCircle} from 'iconsax-react-native'; 
+import { fontType, colors } from '../theme';
+
+const Tab = createBottomTabNavigator();
+const Stack = createStackNavigator();
+function MainApp() {
+  return (
+    <Tab.Navigator
+      screenOptions={{
+        tabBarHideOnKeyboard: true,
+        tabBarActiveTintColor: colors.brown(),
+        tabBarInactiveTintColor: colors.black(),
+        tabBarStyle: {
+          paddingBottom: 10,
+          paddingTop: 10,
+          height: 60,
+        },
+        tabBarLabelStyle: {
+          marginTop: 5,
+          fontSize: 10,
+          fontFamily: fontType['Pjs-Medium'],
+        },
+      }}>
+      <Tab.Screen
+        name="Home"
+        component={Home}
+        options={{
+          tabBarLabel: 'Home',
+          tabBarIcon: ({focused, color}) => (
+            <Home2
+              color={color}
+              variant={focused ? 'Bold' : 'Linear'}
+              size={24}
+            />
+          ),
+          headerShown: false,
+        }}
+      />
+      <Tab.Screen
+        name="Bookmark"
+        component={Bookmark}
+        options={{
+          tabBarLabel: 'Bookmark',
+          tabBarIcon: ({focused, color}) => (
+            <Bookmark2
+              color={color}
+              variant={focused ? 'Bold' : 'Linear'}
+              size={24}
+            />
+          ),
+          headerShown: false,
+        }}
+      />
+      <Tab.Screen
+        name="Tulis"
+        component={Tulis}
+        options={{
+          tabBarLabel: 'Tulis',
+          tabBarIcon: ({focused, color}) => (
+            <Edit
+              color={color}
+              variant={focused ? 'Bold' : 'Linear'}
+              size={24}
+            />
+          ),
+          headerShown: false,
+        }}
+      />
+      <Tab.Screen
+        name="Profile"
+        component={Profile}
+        options={{
+          tabBarLabel: 'Profile',
+          tabBarIcon: ({focused, color}) => (
+            <ProfileCircle
+              color={color}
+              variant={focused ? 'Bold' : 'Linear'}
+              size={24}
+            />
+          ),
+          headerShown: false,
+        }}
+      />
+      
+    </Tab.Navigator>
+  );
+}
+const Router = () => {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen
+        name="MainApp"
+        component={MainApp}
+        options={{headerShown: false}}
+      />
+      <Stack.Screen
+        name="BlogDetail"
+        component={BlogDetail}
+        options={{
+          headerShown: false, 
+          animationEnabled: true,
+          animationTypeForReplace: 'pop',
+          gestureEnabled: true,
+          gestureDirection : 'horizontal',
+          ...TransitionPresets.SlideFromRightIOS,
+        }}
+      />
+      <Stack.Screen
+        name="SearchPage"
+        component={Search}
+        options={{
+          headerShown: false,
+          presentation: 'transparentModal',
+        }}
+      />
+<Stack.Screen
+        name="AddBlog"
+        component={AddBlogForm}
+        options={{
+          headerShown: false,
+          animationEnabled: true,
+          animationTypeForReplace: 'pop',
+          gestureEnabled: true,
+          gestureDirection : 'horizontal',
+          ...TransitionPresets.SlideFromRightIOS,
+        }}
+      />
+      <Stack.Screen
+        name="EditBlog"
+        component={EditBlogForm}
+        options={{
+          headerShown: false,
+          animationEnabled: true,
+          animationTypeForReplace: 'pop',
+          gestureEnabled: true,
+          gestureDirection: 'horizontal',
+          ...TransitionPresets.SlideFromRightIOS,
+        }}
+      />
+    </Stack.Navigator>
+  );
+};
+export default Router;
---
+++ ICook Bab 9/src/screens/Profile/index.js
@@ -0,0 +1,180 @@
+import {ScrollView, StyleSheet, Text, View, TouchableOpacity, ActivityIndicator, RefreshControl} from 'react-native';
+import {Edit, Setting2} from 'iconsax-react-native';
+import React, { useState, useCallback} from 'react';
+import FastImage from 'react-native-fast-image';
+import {ProfileData} from '../../../data';
+import {ItemSmall} from '../../components';
+import {useNavigation, useFocusEffect} from '@react-navigation/native';
+import {fontType, colors} from '../../theme';
+import {formatNumber} from '../../utils/formatNumber';
+import axios from 'axios';
+
+const Profile = () => {
+  const navigation = useNavigation();
+  const [loading, setLoading] = useState(true);
+  const [blogData, setBlogData] = useState([]);
+  const [refreshing, setRefreshing] = useState(false);
+  const getDataBlog = async () => {
+    try {
+      const response = await axios.get(
+        'https://656ed06a6529ec1c6236b0d4.mockapi.io/icookapp/blog',
+      );
+      setBlogData(response.data);
+      setLoading(false)
+    } catch (error) {
+        console.error(error);
+    }
+  };
+
+  const onRefresh = useCallback(() => {
+    setRefreshing(true);
+    setTimeout(() => {
+      getDataBlog()
+      setRefreshing(false);
+    }, 1500);
+  }, []);
+
+  useFocusEffect(
+    useCallback(() => {
+      getDataBlog();
+    }, [])
+  );
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <TouchableOpacity>
+          <Setting2 color={colors.brown()} variant="Linear" size={24} />
+        </TouchableOpacity>
+      </View>
+      <ScrollView
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={{
+          paddingHorizontal: 24,
+          gap: 10,
+          paddingVertical: 20,
+        }} refreshControl={
+          <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
+        }>
+        <View style={{gap: 15, alignItems: 'center'}}>
+          <FastImage
+            style={profile.pic}
+            source={{
+              uri: ProfileData.profilePict,
+              headers: {Authorization: 'someAuthToken'},
+              priority: FastImage.priority.high,
+            }}
+            resizeMode={FastImage.resizeMode.cover}
+          />
+          <View style={{gap: 5, alignItems: 'center'}}>
+            <Text style={profile.name}>{ProfileData.name}</Text>
+            <Text style={profile.info}>
+              Share Cooking Skill{ProfileData.createdAt}
+            </Text>
+          </View>
+          <View style={{flexDirection: 'row', gap: 20}}>
+            <View style={{alignItems: 'center', gap: 5}}>
+              <Text style={profile.sum}>{ProfileData.blogPosted}</Text>
+              <Text style={profile.tag}>Posted</Text>
+            </View>
+            <View style={{alignItems: 'center', gap: 5}}>
+              <Text style={profile.sum}>
+                {formatNumber(ProfileData.following)}
+              </Text>
+              <Text style={profile.tag}>Following</Text>
+            </View>
+            <View style={{alignItems: 'center', gap: 5}}>
+              <Text style={profile.sum}>
+                {formatNumber(ProfileData.follower)}
+              </Text>
+              <Text style={profile.tag}>Follower</Text>
+            </View>
+          </View>
+          <TouchableOpacity style={profile.buttonEdit}>
+            <Text style={profile.buttonText}>Edit Profile</Text>
+          </TouchableOpacity>
+        </View>
+      </ScrollView>
+    </View>
+  );
+};
+
+export default Profile;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: colors.choco(),
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
+  },
+  header: {
+    paddingHorizontal: 24,
+    justifyContent: 'flex-end',
+    flexDirection: 'row',
+    alignItems: 'center',
+    height: 52,
+    elevation: 8,
+    paddingTop: 8,
+    paddingBottom: 4,
+  },
+  title: {
+    fontSize: 20,
+    fontFamily: fontType['Pjs-ExtraBold'],
+    color: colors.brown(),
+  },
+  floatingButton: {
+    backgroundColor: colors.blue(),
+    padding: 15,
+    position: 'absolute',
+    bottom: 24,
+    right: 24,
+    borderRadius: 10,
+    shadowColor: colors.blue(),
+    shadowOffset: {
+      width: 0,
+      height: 4,
+    },
+    shadowOpacity: 0.3,
+    shadowRadius: 4.65,
+
+    elevation: 8,
+  },
+});
+const profile = StyleSheet.create({
+  pic: {width: 100, height: 100, borderRadius: 15},
+  name: {
+    color: colors.brown(),
+    fontSize: 20,
+    fontFamily: fontType['Pjs-ExtraBold'],
+  },
+  info: {
+    fontSize: 12,
+    fontFamily: fontType['Pjs-Regular'],
+    color: colors.grey(),
+  },
+  sum: {
+    fontSize: 16,
+    fontFamily: fontType['Pjs-SemiBold'],
+    color: colors.brown(),
+  },
+  tag: {
+    fontSize: 14,
+    fontFamily: fontType['Pjs-Regular'],
+    color: colors.grey(0.5),
+  },
+  buttonEdit: {
+    paddingHorizontal: 16,
+    paddingVertical: 14,
+    backgroundColor: colors.grey(0.1),
+    borderRadius: 10,
+  },
+  buttonText: {
+    fontSize: 14,
+    fontFamily: fontType['Pjs-SemiBold'],
+    color: colors.brown(),
+  },
+});
